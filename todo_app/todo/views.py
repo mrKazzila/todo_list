baa22d0fc294edd7b3aadc44136a43a753ef86a4
kazila <mrkazzila@gmail.com>
2023-02-19 02:20:38 +0300

    feat: create view for creating todo

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
+
+from todo.forms import TodoCreateForm
 
 
 class IndexView(TemplateView):
@@ -7,3 +10,14 @@ class IndexView(TemplateView):
 
 class TodoView(TemplateView):
     template_name = 'todo/index.html'
+
+
+def create_todo_view(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoCreateForm()})
+    else:
+        form = TodoCreateForm(request.POST)
+        new_todo = form.save(commit=False)
+        new_todo.user = request.user
+        new_todo.save()
+        return redirect('todos:todo')
