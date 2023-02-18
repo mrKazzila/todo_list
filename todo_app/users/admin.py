f383c1c11deeecb2ea1ba79c93c099b19928ec44
kazila <mrkazzila@gmail.com>
2023-02-19 02:15:57 +0300

    feat: delete admin settings

--- todo_app/users/admin.py
+++ todo_app/users/admin.py
@@ -1,10 +1,2 @@
-from django.contrib import admin
 
-# Register your models here.
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
 
