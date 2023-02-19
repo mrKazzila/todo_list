9a1e7599c439372acab506fa3c03a021e4c430e1
kazila <mrkazzila@gmail.com>
2023-02-19 15:01:20 +0300

    refactor: update admin settings for todo

--- todo_app/todo/admin.py
+++ todo_app/todo/admin.py
@@ -5,5 +5,5 @@ from todo.models import Todo
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'important', 'memo', 'date_completed',)
+    list_display = ('pk', 'user', 'title', 'important', 'memo', 'date_completed')
     readonly_fields = ('created', )
