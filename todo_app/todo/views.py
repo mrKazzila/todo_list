14379070b071ae1558a72dcd077833d7decc1411
kazila <mrkazzila@gmail.com>
2023-02-19 11:14:39 +0300

    feat: create view for current user todos

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
-
+from django.views.generic.list import ListView
 from todo.forms import TodoCreateForm
+from todo.models import Todo
 
 
 class IndexView(TemplateView):
     template_name = 'todo/index.html'
 
 
-class TodoView(TemplateView):
-    template_name = 'todo/index.html'
+class TodoListView(ListView):
+    template_name = 'todo/current.html'
+    queryset = Todo.objects.all()
+    ordering = ('-important',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            user=self.request.user,
+            date_completed__isnull=True,
+        )
 
 
 def create_todo_view(request):
@@ -20,4 +30,4 @@ def create_todo_view(request):
         new_todo = form.save(commit=False)
         new_todo.user = request.user
         new_todo.save()
-        return redirect('todos:todo')
+        return redirect('todos:current')
