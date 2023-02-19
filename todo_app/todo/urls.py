06f0e7ec20463e12ecdb65b2c8bed91200e5f7f3
kazila <mrkazzila@gmail.com>
2023-02-19 11:14:48 +0300

    feat: create url for current user todos

--- todo_app/todo/urls.py
+++ todo_app/todo/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from todo.views import TodoView, create_todo_view
+from todo.views import TodoListView, create_todo_view
 
 app_name = 'todos'
 
 urlpatterns = [
-    path('', TodoView.as_view(), name='todo'),
+    path('', TodoListView.as_view(), name='current'),
     path('create', create_todo_view, name='create'),
 ]
