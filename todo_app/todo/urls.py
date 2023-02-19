506481102d074581167cada759436a16dc141dfb
kazila <mrkazzila@gmail.com>
2023-02-19 14:58:57 +0300

    feat: create urls for crud features

--- todo_app/todo/urls.py
+++ todo_app/todo/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from todo.views import TodoListView, create_todo_view
+from todo.views import (CompletedTodoListView, TodoListView, compete_todo_view,
+                        create_todo_view, delete_todo_view, todo_view)
 
 app_name = 'todos'
 
 urlpatterns = [
     path('', TodoListView.as_view(), name='current'),
     path('create', create_todo_view, name='create'),
+    path('completed', CompletedTodoListView.as_view(), name='completed'),
+    path('todo/<int:todo_pk>/', todo_view, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', compete_todo_view, name='complete'),
+    path('todo/<int:todo_pk>/delete', delete_todo_view, name='delete'),
 ]
