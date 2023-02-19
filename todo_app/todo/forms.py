7076e862bbf52440df0cf482f8210ff984d8cef5
kazila <mrkazzila@gmail.com>
2023-02-19 19:51:18 +0300

    refactor: update form

--- todo_app/todo/forms.py
+++ todo_app/todo/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.forms import ModelForm
 
 from todo.models import Todo
@@ -8,17 +7,3 @@ class TodoCreateForm(ModelForm):
     class Meta:
         model = Todo
         fields = ('title', 'memo', 'important')
-
-    title = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control py-4'},
-        ),
-    )
-    memo = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control py-4'},
-        ),
-    )
-    important = forms.BooleanField(
-        required=False,
-    )
