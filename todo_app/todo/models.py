d21908c8495c1106befca54463e0a156bc74beb3
kazila <mrkazzila@gmail.com>
2023-02-21 21:54:22 +0300

    refactor: one field one line for model

--- todo_app/todo/models.py
+++ todo_app/todo/models.py
@@ -3,12 +3,27 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100)
-    memo = models.TextField(blank=True, max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
-    date_completed = models.DateTimeField(null=True, blank=True)
-    important = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(
+        max_length=100,
+    )
+    memo = models.TextField(
+        blank=True,
+        max_length=1000,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    date_completed = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    important = models.BooleanField(
+        default=False,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return f'{self.title=}'
