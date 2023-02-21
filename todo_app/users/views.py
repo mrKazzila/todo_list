ba6e7cc72d34a275f83ab5434f5f05defb0b95b0
kazila <mrkazzila@gmail.com>
2023-02-21 21:51:42 +0300

    refactor: delete unused class

--- todo_app/users/views.py
+++ todo_app/users/views.py
@@ -9,25 +9,15 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 
-class TitleMixin:
-    title = None
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.title
-        return context
-
-
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
     form_class = AuthenticationForm
 
 
-class UserRegistrationCreateView(TitleMixin, SuccessMessageMixin, CreateView):
+class UserRegistrationCreateView(SuccessMessageMixin, CreateView):
     model = User
     form_class = UserCreationForm
     template_name = 'users/registration.html'
-    title = 'Registration'
     success_message = 'Success registration!'
 
     def get_success_url(self):
